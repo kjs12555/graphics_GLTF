d52b8649b79e56d0bb2c008db870600c21064511
ElegantUniv <junho@kookmin.ac.kr>
2019-03-19 22:41:17 +0900

    Create CG_Lab.01.hello_triangle

---
+++ lab/01.Hello_Triangle/README.txt
@@ -0,0 +1,3 @@
+﻿./CG_Lab.01.hello_triangle.pdf	기초 실습 문서
+./CG_Lab.01.hello_triangle_adv.pdf	심화 이론/실습 문서
+./cpp				실습용 연습 코드
---
+++ lab/01.Hello_Triangle/cpp/skeleton_codes/main.cpp
@@ -0,0 +1,184 @@
+///// main.cpp
+///// OpenGL 3+, GLSL 1.20, GLEW, GLFW3
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <cassert>
+
+////////////////////////////////////////////////////////////////////////////////
+/// 쉐이더 관련 변수 및 함수
+////////////////////////////////////////////////////////////////////////////////
+GLuint  program;          // 쉐이더 프로그램 객체의 레퍼런스 값
+GLint   loc_a_position;   // attribute 변수 a_position 위치
+GLint   loc_a_color;      // attribute 변수 a_color 위치
+GLint   loc_u_PVM;        // uniform 변수 u_PVM 위치
+
+GLuint  position_buffer;  // GPU 메모리에서 position_buffer의 위치
+GLuint  color_buffer;     // GPU 메모리에서 color_buffer의 위치
+
+GLuint create_shader_from_file(const std::string& filename, GLuint shader_type);
+void init_shader_program();
+////////////////////////////////////////////////////////////////////////////////
+
+
+////////////////////////////////////////////////////////////////////////////////
+/// 렌더링 관련 변수 및 함수
+////////////////////////////////////////////////////////////////////////////////
+// per-vertex 3D positions (x, y, z)
+GLfloat g_position[] = {
+  0.5f,  0.5f,  0.0f,          // 0th vertex position
+  -0.5f, -0.5f,  0.0f,          // 1st vertex position
+  0.5f, -0.5f,  0.0f,          // 2nd vertex position
+};
+
+// per-vertex RGB color (r, g, b)
+GLfloat g_color[] = {
+  1.0f, 0.0f, 0.0f,             // 0th vertex color (red)
+  1.0f, 0.0f, 0.0f,             // 1st vertex color (red)
+  1.0f, 0.0f, 0.0f,             // 2nd vertex color (red)
+};
+
+void init_buffer_objects();     // VBO init 함수: GPU의 VBO를 초기화하는 함수.
+void render_object();           // rendering 함수: 물체(삼각형)를 렌더링하는 함수.
+////////////////////////////////////////////////////////////////////////////////
+
+
+// GLSL 파일을 읽어서 컴파일한 후 쉐이더 객체를 생성하는 함수
+GLuint create_shader_from_file(const std::string& filename, GLuint shader_type)
+{
+  GLuint shader = 0;
+
+  shader = glCreateShader(shader_type);
+
+  std::ifstream shader_file(filename.c_str());
+  std::string shader_string;
+
+  shader_string.assign(
+    (std::istreambuf_iterator<char>(shader_file)),
+    std::istreambuf_iterator<char>());
+
+  const GLchar * shader_src = shader_string.c_str();
+  glShaderSource(shader, 1, (const GLchar **)&shader_src, NULL);
+  glCompileShader(shader);
+
+  return shader;
+}
+
+// vertex shader와 fragment shader를 링크시켜 program을 생성하는 함수
+void init_shader_program()
+{
+  GLuint vertex_shader
+    = create_shader_from_file("./shader/vertex.glsl", GL_VERTEX_SHADER);
+
+  std::cout << "vertex_shader id: " << vertex_shader << std::endl;
+  assert(vertex_shader != 0);
+
+  GLuint fragment_shader
+    = create_shader_from_file("./shader/fragment.glsl", GL_FRAGMENT_SHADER);
+
+  std::cout << "fragment_shader id: " << fragment_shader << std::endl;
+  assert(fragment_shader != 0);
+
+  program = glCreateProgram();
+  glAttachShader(program, vertex_shader);
+  glAttachShader(program, fragment_shader);
+  glLinkProgram(program);
+
+  std::cout << "program id: " << program << std::endl;
+  assert(program != 0);
+
+  loc_u_PVM = glGetUniformLocation(program, "u_PVM");
+
+  loc_a_position = glGetAttribLocation(program, "a_position");
+  loc_a_color = glGetAttribLocation(program, "a_color");
+}
+
+void init_buffer_objects()
+{
+  /////////////////////////////////////////////////////////////////////
+  /// TODO: CPU 메모리에서 GPU 메모리로 물체의 데이터를 전송하는 부분 - BEGIN
+  /////////////////////////////////////////////////////////////////////
+  
+
+  /////////////////////////////////////////////////////////////////////
+  /// TODO: CPU 메모리에서 GPU 메모리로 물체의 데이터를 전송하는 부분 - END
+  /////////////////////////////////////////////////////////////////////
+}
+
+
+// object rendering: 현재 scene은 삼각형 하나로 구성되어 있음.
+void render_object()
+{
+  // 특정 쉐이더 프로그램 사용
+  glUseProgram(program);
+
+  /////////////////////////////////////////////////////////////////////
+  /// TODO: GPU에 이미 전송된 데이터로 물체를 그리는 부분 - BEGIN
+  /////////////////////////////////////////////////////////////////////
+
+
+
+  /////////////////////////////////////////////////////////////////////
+  /// TODO: GPU에 이미 전송된 데이터로 물체를 그리는 부분 - END
+  /////////////////////////////////////////////////////////////////////
+
+  // 쉐이더 프로그램 사용해제
+  glUseProgram(0);
+}
+
+
+int main(void)
+{
+  GLFWwindow* window;
+
+  // Initialize GLFW library
+  if (!glfwInit())
+    return -1;
+
+  // Create a GLFW window containing a OpenGL context
+  window = glfwCreateWindow(500, 500, "Hello Triangle", NULL, NULL);
+  if (!window)
+  {
+    glfwTerminate();
+    return -1;
+  }
+
+  // Make the current OpenGL context as one in the window
+  glfwMakeContextCurrent(window);
+
+  // Initialize GLEW library
+  if (glewInit() != GLEW_OK)
+    std::cout << "GLEW Init Error!" << std::endl;
+
+  // Print out the OpenGL version supported by the graphics card in my PC
+  std::cout << glGetString(GL_VERSION) << std::endl;
+
+  init_shader_program();
+
+  // TODO: GPU의 VBO를 초기화하는 함수 호출
+  
+
+  // Loop until the user closes the window
+  while (!glfwWindowShouldClose(window))
+  {
+    glClearColor(0.5f, 0.5f, 0.5f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    // TODO: 물체(삼각형)를 렌더링하는 함수 호출
+    
+
+    // Swap front and back buffers
+    glfwSwapBuffers(window);
+
+    // Poll for and process events
+    glfwPollEvents();
+  }
+
+  glfwTerminate();
+
+  return 0;
+}
