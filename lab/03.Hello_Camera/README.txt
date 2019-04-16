54a0d372b3c1d22522a1bd6ee92d94a9c7e3bdba
ElegantUniv <junho@kookmin.ac.kr>
2019-04-16 16:01:05 +0900

    Uploading Lab 03

---
+++ lab/03.Hello_Camera/README.txt
@@ -0,0 +1,6 @@
+﻿./main.cpp			실습용 코드
+./Camera.h			카메라 클래스 헤더파일
+./Camera.cpp			카메라 클래스 소스파일
+./Makefile			Makefile
+./skeleton_codes			연습 코드가 있는 디렉토리
+./complete_codes			완성된 코드가 있는 디렉토리
---
+++ lab/03.Hello_Camera/complete_codes/main.cpp
@@ -0,0 +1,311 @@
+///// main.cpp
+///// OpenGL 3+, GLSL 1.20, GLEW, GLFW3
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <cassert>
+#include "../common/vec.hpp"
+#include "../common/transform.hpp"
+#include "Camera.h"
+
+////////////////////////////////////////////////////////////////////////////////
+/// 쉐이더 관련 변수 및 함수
+////////////////////////////////////////////////////////////////////////////////
+GLuint  program;          // 쉐이더 프로그램 객체의 레퍼런스 값
+GLint   loc_a_position;   // attribute 변수 a_position 위치
+GLint   loc_a_color;      // attribute 변수 a_color 위치
+GLint   loc_u_PVM;        // uniform 변수 u_PVM 위치
+
+GLuint  position_buffer;  // GPU 메모리에서 position_buffer의 위치
+GLuint  color_buffer;     // GPU 메모리에서 color_buffer의 위치
+
+GLuint create_shader_from_file(const std::string& filename, GLuint shader_type);
+void init_shader_program();
+void init_buffer_objects();
+////////////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////////////////
+/// 변환 관련 변수 및 함수
+////////////////////////////////////////////////////////////////////////////////
+kmuvcl::math::mat4x4f     mat_model, mat_view, mat_proj;
+kmuvcl::math::mat4x4f     mat_PVM;
+
+float   g_translate_x = 0.0, g_translate_y = 0.0, g_translate_z = 0.0;
+bool    g_is_animation = false;
+
+void set_transform();
+////////////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////////////////
+/// 렌더링 관련 변수 및 함수
+////////////////////////////////////////////////////////////////////////////////
+// per-vertex 3D positions (x, y, z)
+GLfloat g_position[] = {
+   0.5f,  0.5f,  0.0f,          // 0th vertex position
+  -0.5f, -0.5f,  0.0f,          // 1st vertex position
+   0.5f, -0.5f,  0.0f,          // 2nd vertex position
+};
+
+// per-vertex RGB color (r, g, b)
+GLfloat g_color[] = {
+  1.0f, 0.0f, 0.0f,             // 0th vertex color (red)
+  1.0f, 0.0f, 0.0f,             // 1st vertex color (red)
+  1.0f, 0.0f, 0.0f,             // 2nd vertex color (red)
+};
+
+void render_object();           // rendering 함수: 물체(삼각형)를 렌더링하는 함수.
+////////////////////////////////////////////////////////////////////////////////
+
+
+////////////////////////////////////////////////////////////////////////////////
+/// 카메라 및 뷰포트 관련 변수
+////////////////////////////////////////////////////////////////////////////////
+Camera  camera;
+float   g_aspect = 1.0f;
+////////////////////////////////////////////////////////////////////////////////
+
+
+// GLSL 파일을 읽어서 컴파일한 후 쉐이더 객체를 생성하는 함수
+GLuint create_shader_from_file(const std::string& filename, GLuint shader_type)
+{
+  GLuint shader = 0;
+
+  shader = glCreateShader(shader_type);
+
+  std::ifstream shader_file(filename.c_str());
+  std::string shader_string;
+
+  shader_string.assign(
+    (std::istreambuf_iterator<char>(shader_file)),
+    std::istreambuf_iterator<char>());
+
+  const GLchar * shader_src = shader_string.c_str();
+  glShaderSource(shader, 1, (const GLchar **)&shader_src, NULL);
+  glCompileShader(shader);
+
+  return shader;
+}
+
+// vertex shader와 fragment shader를 링크시켜 program을 생성하는 함수
+void init_shader_program()
+{
+  GLuint vertex_shader
+    = create_shader_from_file("./shader/vertex.glsl", GL_VERTEX_SHADER);
+
+  std::cout << "vertex_shader id: " << vertex_shader << std::endl;
+  assert(vertex_shader != 0);
+
+  GLuint fragment_shader
+    = create_shader_from_file("./shader/fragment.glsl", GL_FRAGMENT_SHADER);
+
+  std::cout << "fragment_shader id: " << fragment_shader << std::endl;
+  assert(fragment_shader != 0);
+
+  program = glCreateProgram();
+  glAttachShader(program, vertex_shader);
+  glAttachShader(program, fragment_shader);
+  glLinkProgram(program);
+
+  std::cout << "program id: " << program << std::endl;
+  assert(program != 0);
+
+  loc_u_PVM = glGetUniformLocation(program, "u_PVM");
+
+  loc_a_position = glGetAttribLocation(program, "a_position");
+  loc_a_color = glGetAttribLocation(program, "a_color");
+}
+
+void init_buffer_objects()
+{
+  glGenBuffers(1, &position_buffer);
+  glBindBuffer(GL_ARRAY_BUFFER, position_buffer);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(g_position), g_position, GL_STATIC_DRAW);
+
+  glGenBuffers(1, &color_buffer);
+  glBindBuffer(GL_ARRAY_BUFFER, color_buffer);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(g_color), g_color, GL_STATIC_DRAW);
+}
+
+void set_transform()
+{
+  // set camera transformation
+  kmuvcl::math::vec3f eye     = camera.position();
+  kmuvcl::math::vec3f up      = camera.up_direction();
+  kmuvcl::math::vec3f center  = eye + camera.front_direction();
+
+  std::cout << "eye:    " << eye << std::endl;
+  std::cout << "up:     " << up << std::endl;
+  std::cout << "center: " << center << std::endl;
+
+  mat_view = kmuvcl::math::lookAt(eye[0], eye[1], eye[2],
+                                  center[0], center[1], center[2],
+                                  up[0], up[1], up[2]);
+
+  float n = camera.near();
+  float f = camera.far();
+
+  if (camera.mode() == Camera::kOrtho)
+  {
+    float l = camera.left();
+    float r = camera.right();
+    float b = camera.bottom();
+    float t = camera.top();
+
+    std::cout << "(camera.mode() == Camera::kOrtho)" << std::endl;
+    std::cout << "(l, r, b, t, n, f): " << l << ", " << r << ", " << b << ", " << t << ", " << n << ", " << f << std::endl;
+
+    mat_proj = kmuvcl::math::ortho(l, r, b, t, n, f);
+  }
+  else if (camera.mode() == Camera::kPerspective)
+  {
+    std::cout << "(camera.mode() == Camera::kPerspective)" << std::endl;
+    std::cout << "(fov, aspect, n, f): " << camera.fovy() << ", " << g_aspect << ", " << n << ", " << f << std::endl;
+
+    mat_proj = kmuvcl::math::perspective(camera.fovy(), g_aspect, n, f);
+  }
+
+  // set object transformation
+  mat_model = kmuvcl::math::translate(g_translate_x, g_translate_y, g_translate_z);
+}
+
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+  if (key == GLFW_KEY_H && action == GLFW_PRESS)
+    g_translate_x -= 0.1f;
+  if (key == GLFW_KEY_L && action == GLFW_PRESS)
+    g_translate_x += 0.1f;
+  if (key == GLFW_KEY_J && action == GLFW_PRESS)
+    g_translate_y -= 0.1f;
+  if (key == GLFW_KEY_K && action == GLFW_PRESS)
+    g_translate_y += 0.1f;
+
+  if (key == GLFW_KEY_P && action == GLFW_PRESS)
+  {
+    g_is_animation = !g_is_animation;
+    std::cout << (g_is_animation ? "animation" : "no animation") << std::endl;
+  }
+  
+  /// TODO: Keyboard settings for Camera
+  if (key == GLFW_KEY_A && action == GLFW_PRESS)
+    camera.move_left(0.1f);
+  if (key == GLFW_KEY_D && action == GLFW_PRESS)
+    camera.move_right(0.1f);
+  if (key == GLFW_KEY_W && action == GLFW_PRESS)
+    camera.move_forward(0.1f);
+  if (key == GLFW_KEY_S && action == GLFW_PRESS)
+    camera.move_backward(0.1f);
+  
+  if (key == GLFW_KEY_Q && action == GLFW_PRESS)
+  {
+    camera.set_mode(camera.mode() == Camera::kOrtho ? Camera::kPerspective : Camera::kOrtho);
+  }    
+}
+
+void frambuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+  glViewport(0, 0, width, height);
+
+  g_aspect = (float)width / (float)height;
+}
+
+
+// object rendering: 현재 scene은 삼각형 하나로 구성되어 있음.
+void render_object()
+{
+  glClearColor(0.5f, 0.5f, 0.5f, 1.0f);
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+  // 특정 쉐이더 프로그램 사용
+  glUseProgram(program);
+
+  // Setting Proj * View * Model
+  mat_PVM = mat_proj * mat_view * mat_model;
+  glUniformMatrix4fv(loc_u_PVM, 1, GL_FALSE, mat_PVM);
+
+  // 앞으로 언급하는 배열 버퍼(GL_ARRAY_BUFFER)는 position_buffer로 지정
+  glBindBuffer(GL_ARRAY_BUFFER, position_buffer);
+  // 버텍스 쉐이더의 attribute 중 a_position 부분 활성화
+  glEnableVertexAttribArray(loc_a_position);
+  // 현재 배열 버퍼에 있는 데이터를 버텍스 쉐이더 a_position에 해당하는 attribute와 연결
+  glVertexAttribPointer(loc_a_position, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
+
+  // 앞으로 언급하는 배열 버퍼(GL_ARRAY_BUFFER)는 color_buffer로 지정
+  glBindBuffer(GL_ARRAY_BUFFER, color_buffer);
+  // 버텍스 쉐이더의 attribute 중 a_color 부분 활성화
+  glEnableVertexAttribArray(loc_a_color);
+  // 현재 배열 버퍼에 있는 데이터를 버텍스 쉐이더 a_color에 해당하는 attribute와 연결
+  glVertexAttribPointer(loc_a_color, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
+
+  // 삼각형 그리기
+  glDrawArrays(GL_TRIANGLES, 0, 3);
+
+  // 정점 attribute 배열 비활성화
+  glDisableVertexAttribArray(loc_a_position);
+  glDisableVertexAttribArray(loc_a_color);
+  // 쉐이더 프로그램 사용해제
+  glUseProgram(0);
+}
+
+
+int main(void)
+{
+  GLFWwindow* window;
+
+  // Initialize GLFW library
+  if (!glfwInit())
+    return -1;
+
+  // Create a GLFW window containing a OpenGL context
+  window = glfwCreateWindow(500, 500, "Hello Triangle", NULL, NULL);
+  if (!window)
+  {
+    glfwTerminate();
+    return -1;
+  }
+
+  // Make the current OpenGL context as one in the window
+  glfwMakeContextCurrent(window);
+
+  // Initialize GLEW library
+  if (glewInit() != GLEW_OK)
+    std::cout << "GLEW Init Error!" << std::endl;
+
+  // Print out the OpenGL version supported by the graphics card in my PC
+  std::cout << glGetString(GL_VERSION) << std::endl;
+
+  init_shader_program();
+  init_buffer_objects();
+
+  glfwSetKeyCallback(window, key_callback);
+  /// TODO: Set a callback for resizing the framebuffer 
+  glfwSetFramebufferSizeCallback(window, frambuffer_size_callback);
+
+  // Loop until the user closes the window
+  while (!glfwWindowShouldClose(window))
+  {
+    // Poll for and process events
+    glfwPollEvents();
+
+    set_transform();
+    render_object();
+
+    if (g_is_animation)
+    {
+      g_translate_x += 0.1f;
+      if (g_translate_x > 1.0f)
+        g_translate_x = -1.0f;
+    }
+
+    // Swap front and back buffers
+    glfwSwapBuffers(window);
+  }
+
+  glfwTerminate();
+
+  return 0;
+}
